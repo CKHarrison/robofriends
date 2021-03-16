c772fec01026d2447cde272c0b66dc97cb435d76
CKHarrison <hirohumi1882@gmail.com>
2021-03-16 01:13:19 -0700

    Finish initial version of app

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    }
+  }
+
+  // check for error upon lifecycle rendering child prop
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if(this.state.hasError === true) {
+      return <h1>Ooops. That is not good</h1>
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) =>  {
+    // setting the state of searchfield to the event value.
+    this.setState({searchfield: event.target.value})
+    
+  }
+
+  // Triggers when component mounts, so the user can be fetched from the api using promises mocked by json placeholder
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users =>  this.setState({robots: users}))
+  }
+
+  render() {
+    // Destructuring to get robots and searchfield from state
+    const {robots, searchfield} = this.state;
+    // Filtering the robots based on the search input
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    
+    // Check to see if robots have been returned from the fetch and handle it
+     return !robots.length  ?
+     <h1 className='tc'>Loading</h1> :
+       (
+        <div className='tc'>
+          <h1 className='f1'>Robot Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+               <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+export default App;
