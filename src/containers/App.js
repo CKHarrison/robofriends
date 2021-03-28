9263a64259435189b72bec804da7205e2f21724e
CKHarrison <hirohumi1882@gmail.com>
2021-03-28 10:21:50 -0700

    swapped from react hooks to redux store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,49 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
+import {requestRobots, setSearchField} from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
-
-  onSearchChange = (event) =>  {
-    // setting the state of searchfield to the event value.
-    this.setState({searchfield: event.target.value})
-    
-  }
 
   // Triggers when component mounts, so the user can be fetched from the api using promises mocked by json placeholder
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users =>  this.setState({robots: users}))
+    this.props.onRequestRobots();
   }
 
   render() {
-    // Destructuring to get robots and searchfield from state
-    const {robots, searchfield} = this.state;
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     // Filtering the robots based on the search input
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     
     // Check to see if robots have been returned from the fetch and handle it
-     return !robots.length  ?
+     return isPending  ?
      <h1 className='tc'>Loading</h1> :
        (
         <div className='tc'>
           <h1 className='f1'>Robot Friends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundry>
                <CardList robots={filteredRobots} />
@@ -51,5 +53,5 @@ class App extends Component {
       );
   }
 }
-
-export default App;
+// wrap connect around app. HOC
+export default connect(mapStateToProps, mapDispatchToProps)(App);
